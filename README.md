89c6954a0b475574de2cdba7f0f68a8ae5cd6869
Fadta <fvpan01@gmail.com>
2022-03-28 18:28:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Archive Watcher
+Command line utility for backups.
+
+Add a file or directory to a watch list.
+
+Make a copy of all of the watched files
+and directories on the watch list
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    # Package info
+    name="archive-watcher",
+    version="0.0.1",
+    url="https://github.com/Fadta/archive-watcher",
+    license="LICENSE",
+
+    # Author
+    author="Francisco Nahuel Vicente",
+    author_email="fvpan01@gmail.com",
+
+    packages=["archive_watcher"],
+)
