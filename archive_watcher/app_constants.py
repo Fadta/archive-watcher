5ae63ec84a685f776eff9496a3fea97efb49cefb
Fadta <fvpan01@gmail.com>
2022-03-30 15:52:34 -0300

    created more global constants for organization

--- archive_watcher/app_constants.py
+++ archive_watcher/app_constants.py
@@ -1,3 +1,7 @@
-DEFAULT_BACKUP_FOLDER = ""
+import os.path
+
+APP_FOLDER = "~/.config/archive_watcher/"
+WATCHLIST_FOLDER = os.path.join(APP_FOLDER, "watchlists/")
+DEFAULT_BACKUP_FOLDER = os.path.join(APP_FOLDER, "backups/")
 
 DEFAULT_WATCHLIST = "default"
