7afdfe746513287f64e87efe33bed3600bac5523
Fadta <fvpan01@gmail.com>
2022-03-30 15:53:07 -0300

    created more exceptions and organized exception hierarchy

--- archive_watcher/app_exceptions.py
+++ archive_watcher/app_exceptions.py
@@ -1,5 +1,20 @@
 class ArchiverException(BaseException):
     pass
 
-class WatchlistNotFoundException(ArchiverException):
+class WatchlistException(ArchiverException):
+    pass
+
+class PathException(ArchiverException):
+    pass
+
+class WatchlistNotFoundException(WatchlistException):
+    pass
+
+class WatchlistExistsException(WatchlistException):
+    pass
+
+class PathNotFoundException(PathException):
+    pass
+
+class UnwatchablePathException(PathException):
     pass
